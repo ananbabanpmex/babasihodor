996c128d21b67e5b6bc1ae92b7fd36cd13e44e54
ananbabanpmex <42073807+ananbabanpmex@users.noreply.github.com>
2019-02-24 19:08:14 +0300

    Update vutu.js

--- vutu.js
+++ vutu.js
@@ -11,4 +11,4 @@ bot.on('guildMemberAdd', member => {
     member.guild.channels.get('438404115757006878').send('Hey ' + '<@' + member.user.id + '>' + ', Discord Sunucumuza Hojjgeldiniz efenim :tada::hugging: !'); 
 });
 
-bot.login('NTQ5MjIxMDI3Njc5NTY3ODg5.D1RL-w.41tHCH28PrQdzmfzGCHOvzIFd1k');
+bot.login('yarramı ye');
